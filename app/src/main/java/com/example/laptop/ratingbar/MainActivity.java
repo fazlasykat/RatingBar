18a10392220184774ac79970b7e6514167a70ab3
fazlasykat <fazlasykat24@gmail.com>
2018-09-13 15:56:08 +0600

    Initial commit

---
+++ app/src/main/java/com/example/laptop/ratingbar/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.laptop.ratingbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingBarId);
+        textView = findViewById(R.id.textViewId);
+
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+
+                textView.setText("You have rated " + v +  " out of 6");
+            }
+        });
+
+    }
+}
